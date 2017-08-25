7f35c8fc65f211a65c941923c39b1fcc7107ebed
Abraham Rodriguez <darkfire493@gmail.com>
2017-08-25 13:09:43 -0500

    FixedQRCodeStuff

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -8,7 +8,7 @@ class QrCodesController < ApplicationController
     @qr = RQRCode::QRCode.new( qr_code_params[:tipo_descuento], size: 8, :level => :h)
     png = @qr.to_img												
     # returns an instance of ChunkyPNG
-    png.resize(450, 450).save("app/assets/images/qrcodes/qr_image#{@tipo_descuento}.png")
+    png.resize(450, 450).save("app/assets/images/qrcodes/qr_image.png")
   end
 
 private
